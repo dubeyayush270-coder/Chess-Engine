8575f8e8f1d1fc96ccde49003cf39bee51ddb323
Ayush Dubey <dubeyayush270@gmail.com>
2026-07-15 00:50:34 +0530

    Draw 8x8 chessboard

--- main.cpp
+++ main.cpp
@@ -10,26 +10,44 @@ int main(int argc, char* argv[])
 	}
 
 	// Create Window
-	std::cout << "Creating Window...\n";
+	//std::cout << "Creating Window...\n";
 	SDL_Window* window = SDL_CreateWindow(
 		"Ayush's Chess Engine",
 		SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED,
-		1000,
-		700,
+		800,
+		800,
 		SDL_WINDOW_SHOWN
 	);
-	std::cout << "Window Created Successfully!\n";
+	//std::cout << "Window Created Successfully!\n";
 
 	if (window == nullptr)
 	{
 		std::cout << "Window Creation Failed : " << SDL_GetError() << std::endl;
 		SDL_Quit();
-		return -1;
+		return 1;
 	}
+
+	// Creating Renderer
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	if (renderer == nullptr)
+	{
+		std::cout << "Renderer could not be created! SDL_error: " << SDL_GetError() << std::endl;
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 1;
+	}
+
+	// Code For Square
+
+	SDL_Rect square;
+
+	// Code For Square
 
 	bool running = true;
 	SDL_Event event;
+
 
 	while (running)
 	{
@@ -40,8 +58,40 @@ int main(int argc, char* argv[])
 				running = false;
 			}
 		}
+
+		// Clear the Screen
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
+
+		//Draw the chess Board
+		
+		for (int row = 0; row < 8; row++) 
+		{
+			for (int column = 0; column < 8; column++) 
+			{
+				square.x = column * 100;
+				square.y = row * 100;
+				square.w = 100;
+				square.h = 100;
+
+				if ((row + column) % 2 == 0) 
+				{
+					SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+				}
+				else 
+				{
+					SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+				}
+				SDL_RenderFillRect(renderer, &square);
+			}
+
+		}
+
+		// Display Everything
+		SDL_RenderPresent(renderer);
 	}
 
+	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	std::cout << "Closing Game...\n";
